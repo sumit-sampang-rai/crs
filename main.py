d0fa7a25a9acb89c15c494d73c253e53ef212b9f
Sumit Rai <4235912+sumit-sampang-rai@users.noreply.github.com>
2024-02-07 18:37:06 +0545

    Adding CRS API

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
+import pandas as pd
+import requests
 import streamlit as st
+
+
+API_URL = "https://www.canada.ca/content/dam/ircc/documents/json/ee_rounds_123_en.json"
+
+
+@st.cache_data
+def cache_score_api():
+    return requests.get(API_URL).json()
+
+df = pd.DataFrame(cache_score_api()["rounds"])
+
+df
